d40fb43af3f90757bf191dd30703e0451eaef38a
Dev05-Telnet <dev05.telnet@gmail.com>
2022-01-13 12:26:22 +0530

    Added Header One

---
+++ react-js/src/components/footer/one.tsx
@@ -0,0 +1,9 @@
+function FooterOne() {
+    return (
+        <footer >
+            This is the footer
+        </footer>
+    );
+}
+
+export default FooterOne;
---
+++ react-js/src/pages/home/index.tsx
@@ -0,0 +1,16 @@
+import FooterOne from "../../components/footer/one";
+import HeaderOne from "../../components/header/one";
+
+function Home() {
+  return (
+    <div >
+      <HeaderOne />
+      <div>
+        This is the Home Page
+      </div>
+      <FooterOne />
+    </div>
+  );
+}
+
+export default Home;
